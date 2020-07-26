c82fc772bf65cc4a799db76184944d39ddce5af6
Ramdhawa <shinlia21@gmail.com>
2020-07-26 11:13:36 +0800

    add tugas 2 js

---
+++ tugas2.js
@@ -0,0 +1,18 @@
+var number=69;
+var string='kocak';
+var boolean=false;
+if(number=69)
+{
+  boolean=true;
+}
+else {
+  boolean=false;
+}
+var kosong="belum null";
+kosong=null;
+var array=[1,"ini array","ini juga array","ini juga sangat array","ini juga sangat hampir array","na ini",96];
+console.log("var number : ",number)
+console.log("var string :",string)
+console.log("var boolean :",boolean)
+console.log("var null :",kosong)
+console.log("var array : ",array[5])
